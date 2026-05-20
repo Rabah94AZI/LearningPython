aebb1a19835cf13181ff289d983a7fcd07d7b5d5
Rabah94AZI <abdouazzi91@gmail.com>
2026-05-20 13:48:32 +0200

    crée le dossier initiation_python pour commencer les premier tp python et séparer main.py dans le dossier trick python

---
+++ initiation_python/tp1.py
@@ -0,0 +1,13 @@
+print("lancer mon premier programme python")
+print("Bonjour")#python va afficher Bonjour a l'ecran
+#il faudra le lancer sur un terminal 
+#nom = input("Quel est votre nom")
+#print("Bonjour"+nom)# si tu as code runner sur vscode ca va pas te donner la main pour interagir 
+#sol : # selectionner la ligne, clique droit puis run dans un terminal 
+#utilisation des variables 
+nom="rabs"
+print(nom)# le programme va récuerer rabs et l'afficher sur l'ecran
+
+#concaténer des chaine 
+print("bonjour "+nom+"!")#le + permet de concat les chaine de caracteres 
+
