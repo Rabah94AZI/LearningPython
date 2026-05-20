b304c5b984a79ab3d467e26cae1ac7a0ec070650
Rabah94AZI <abdouazzi91@gmail.com>
2026-05-20 12:51:45 +0200

    ajouter des commentaires sur le premier code

--- trick/main.py
+++ trick/main.py
@@ -1,4 +1,5 @@
 
+#test sur les fonctions 
 def lo(x):
     if x==1:
         return 1
@@ -8,18 +9,20 @@ def lo(x):
 res=lo(3)
 print(res)
 
+#test sur les listes, 
 w=["python","is","awesoome"]
+#test join retour ?
 print("-".join(w))
 
 z="ami"
-print(z*3)
+print(z*3) #ca doit retourner amiamiami
 
 a=int('11',2)
-print(a)
+print(a)#affichage dans la base 2
 
 
 var = int(8.9)
-print(var)
+print(var)#récupérer la partie entière 
 
 l=[1,2,3,4]
 print("|")
