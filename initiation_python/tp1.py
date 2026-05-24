676138031e557a49f3c450df1ba7f52b71cd226e
Rabah94AZI <abdouazzi91@gmail.com>
2026-05-24 16:10:37 +0200

    des opérations sur les listes

--- initiation_python/tp1.py
+++ initiation_python/tp1.py
@@ -27,3 +27,28 @@ try :
 except:
     print("erreur")
 
+#les listes 
+print("des opérations sur les listes")
+li = [10,20,30,40,50]
+print(li[0])#le premier element :10
+print(li[-1])#le dernier element : 50
+print(li[1:3])#récupérer 20 30
+print(li[:3])# récupére 10 20 30 
+print(li[2:])#les elements a partir de l'indice 2 : 30 40 50
+
+li2=['pomme','banane','cerise']
+print(li2)                       #['pomme','banane','cerise']
+li2.append('pasteque')           #['pomme','banane','cerise','pasteque']
+print(li2)
+li2.insert(1,'ananas')           # ['pomme','ananas','banane','cerise','pasteque']
+print(li2)
+li2.extend(['dates','oignons'])#['pomme','ananas','banane','cerise','pasteque','dates','oignons']
+print(li2)
+li2.remove("dates")#['pomme','ananas','banane','cerise','pasteque','oignons']
+print(li2)
+li2.pop()#['pomme','ananas','banane','cerise','pasteque']
+print(li2)
+li2.sort()#['ananas', 'banane', 'cerise', 'pasteque', 'pomme']
+print(li2)
+li2.reverse()
+print(li2)#['pomme','pasteque','cerise','banane','ananas']
